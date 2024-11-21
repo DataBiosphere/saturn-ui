7a28ed432eb81194490b33ac5bb99a99bd30f7bf
Saloni Shah <salonipshah11@gmail.com>
2024-11-21 18:07:23 -0500

    tests

--- src/libs/ajax/methods/providers/EditMethodProvider.test.ts
+++ src/libs/ajax/methods/providers/EditMethodProvider.test.ts
@@ -42,7 +42,6 @@ describe('EditMethodProvider', () => {
   it('handles create new snapshot call', async () => {
     // Arrange
     mockAjax();
-
     const signal = new window.AbortController().signal;
 
     // Act
