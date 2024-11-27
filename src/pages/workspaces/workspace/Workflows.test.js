4567a00047068d725e40ca8b1b00be56ca9006e1
Saloni Shah <salonipshah11@gmail.com>
2024-11-27 15:45:47 -0500

    new workflow modal and tests

--- src/pages/workspaces/workspace/Workflows.test.js
+++ src/pages/workspaces/workspace/Workflows.test.js
@@ -1,147 +1,92 @@
 import { screen } from '@testing-library/react';
 import { act } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import { h } from 'react-hyperscript-helpers';
-import { Ajax } from 'src/libs/ajax';
-import { goToPath } from 'src/libs/nav';
-import { FindWorkflowModal } from 'src/pages/workspaces/workspace/Workflows';
+import { Apps } from 'src/libs/ajax/leonardo/Apps';
+import { Runtimes } from 'src/libs/ajax/leonardo/Runtimes';
+import { Workspaces } from 'src/libs/ajax/workspaces/Workspaces';
+import { oidcStore } from 'src/libs/state';
+import { Workflows } from 'src/pages/workspaces/workspace/Workflows';
 import { renderWithAppContexts as render } from 'src/testing/test-utils';
 
-jest.mock('src/libs/ajax');
-
+jest.mock('src/libs/ajax/leonardo/Apps');
+jest.mock('src/libs/ajax/leonardo/Runtimes');
+jest.mock('src/libs/ajax/workspaces/Workspaces');
 jest.mock('src/libs/nav', () => ({
   getCurrentUrl: jest.fn().mockReturnValue(new URL('https://app.terra.bio')),
   getLink: jest.fn(),
   goToPath: jest.fn(),
 }));
 
-describe('Find Workflow Modal (GCP)', () => {
-  let getFeaturedMethods;
-  let getRuns;
-  let loadMethod;
-  let methodApi;
-  let configurations;
-  let toWorkspace;
+jest.mock('src/libs/notifications', () => ({
+  notify: jest.fn(),
+}));
 
-  const namespace = 'testNamespace';
-  const name = 'testName';
-  const ws = {
-    name,
-    namespace,
-    cloudPlatform: 'Gcp',
+jest.spyOn(oidcStore, 'get').mockImplementation(
+  jest.fn().mockReturnValue({
+    userManager: { getUser: jest.fn() },
+  })
+);
+
+describe('Workflows', () => {
+  const mockGoogleWorkspace = {
+    accessLevel: 'OWNER',
+    owners: ['groot@gmail.com'],
+    workspace: {
+      attributes: {
+        description: '',
+      },
+      authorizationDomain: [],
+      billingAccount: 'billingAccounts/google-billing-account',
+      bucketName: 'bucket-name',
+      cloudPlatform: 'Gcp',
+      completedCloneWorkspaceFileTransfer: '2024-11-27T22:29:04.319Z',
+      createdBy: 'groot@gmail.com',
+      createdDate: '2024-11-27T22:26:06.124Z',
+      googleProject: 'google-project-id',
+      isLocked: false,
+      lastModified: '2024-11-27T22:26:06.202Z',
+      name: 'groot-scientific-workflow',
+      namespace: 'groot-namespace',
+      workspaceId: 'google-workspace-id',
+      workspaceType: 'rawls',
+      workspaceVersion: 'v2',
+    },
+    canShare: true,
+    canCompute: true,
+    workspaceInitialized: true,
   };
-  const onDismiss = jest.fn();
-  const methodConfiguration = { inputs: {}, outputs: {}, snapshotId: 1, name: 'haplotypecaller-gvcf-gatk4', namespace: 'gatk' };
+  const mockStorageDetails = {
+    fetchedLocation: 'SUCCESS',
+  };
 
-  beforeEach(() => {
-    getFeaturedMethods = jest.fn(() =>
-      Promise.resolve([
-        {
-          namespace: 'gatk',
-          name: 'haplotypecaller-gvcf-gatk4',
-        },
-      ])
-    );
-    getRuns = jest.fn(({ _ }) =>
-      Promise.resolve([
-        {
-          name: 'haplotypecaller-gvcf-gatk4',
-          createDate: '2018-11-30T22:18:35Z',
-          url: 'http://agora.dsde-dev.broadinstitute.org/api/v1/methods/gatk/haplotypecaller-gvcf-gatk4/1',
-          synopsis: 'Runs HaplotypeCaller from GATK4 in GVCF mode on a single sample',
-          entityType: 'Workflow',
-          snapshotComment: '',
-          snapshotId: 1,
-          namespace: 'gatk',
-        },
-        {
-          name: 'unfeatured-workflow',
-          createDate: '2018-11-30T22:18:35Z',
-          url: 'http://agora.dsde-dev.broadinstitute.org/api/v1/methods/gatk/unfeatured-workflow/1',
-          synopsis: 'Not a featured workflow',
-          entityType: 'Workflow',
-          snapshotComment: '',
-          snapshotId: 1,
-          namespace: 'gatk',
-        },
-      ])
-    );
-    loadMethod = jest.fn(() => {
-      Promise.resolve({
-        managers: ['test@test.org'],
-        name: 'haplotypecaller-gvcf-gatk4',
-        createDate: '2018-11-30T22:18:35Z',
-        documentation: 'Some workflow documentation',
-        entityType: 'Workflow',
-        snapshotComment: '',
-        snapshotId: 1,
-        namespace: 'gatk',
-        payload: '## Some workflow content',
-        url: 'http://agora.dsde-dev.broadinstitute.org/api/v1/methods/gatk/haplotypecaller-gvcf-gatk4/1',
-        public: true,
-        synopsis: 'Runs HaplotypeCaller from GATK4 in GVCF mode on a single sample',
-      });
+  const mockAjax = () => {
+    Workspaces.mockReturnValue({
+      workspace: (_namespace, _name) => ({
+        details: jest.fn().mockResolvedValue(mockGoogleWorkspace),
+        checkBucketReadAccess: jest.fn(),
+        storageCostEstimate: jest.fn(),
+        bucketUsage: jest.fn(),
+        checkBucketLocation: jest.fn().mockResolvedValue(mockStorageDetails),
+        listMethodConfigs: jest.fn(),
+      }),
     });
-    methodApi = () => ({
-      get: loadMethod,
-      configs: configurations,
-      toWorkspace,
-    });
-    configurations = jest.fn(() => Promise.resolve([methodConfiguration]));
-    toWorkspace = jest.fn(() => Promise.resolve());
+    Runtimes.mockReturnValue({ listV2: jest.fn() });
+    Apps.mockReturnValue({ list: jest.fn().mockReturnValue([]) });
+  };
 
-    Ajax.mockImplementation(() => ({
-      FirecloudBucket: {
-        getFeaturedMethods,
-      },
-      Methods: {
-        list: getRuns,
-        method: methodApi,
-      },
-      Metrics: {
-        captureEvent: jest.fn(() => {}),
-      },
-    }));
-  });
-
-  it('renders recommended workflows', async () => {
+  it('loads Find Workflow card', async () => {
     // Arrange
-    const props = { namespace, name, ws, onDismiss };
+    const namespace = 'groot-namespace';
+    const name = 'groot-scientific-workflow';
+
+    mockAjax();
 
     // Act
     await act(async () => {
-      render(h(FindWorkflowModal, props));
+      render(h(Workflows, { name, namespace }));
     });
-
-    expect(getFeaturedMethods).toHaveBeenCalled();
-    expect(getRuns).toHaveBeenCalled();
-
-    expect(screen.queryByText('haplotypecaller-gvcf-gatk4')).toBeInTheDocument();
-    expect(screen.queryByText('Runs HaplotypeCaller from GATK4 in GVCF mode on a single sample')).toBeInTheDocument();
-    expect(screen.queryByText('unfeatured-workflow')).not.toBeInTheDocument();
-    expect(screen.queryByText('Not a featured workflow')).not.toBeInTheDocument();
-  });
-
-  it('supports importing recommended workflows', async () => {
-    // Arrange
-    const user = userEvent.setup();
-
-    const props = { namespace, name, ws, onDismiss };
-
-    // Act
-    await act(async () => {
-      render(h(FindWorkflowModal, props));
-    });
-
-    const workflowCardButton = screen.getAllByRole('button').filter((button) => button.textContent.includes('haplotypecaller-gvcf-gatk4'))[0];
-    expect(workflowCardButton).toHaveTextContent('haplotypecaller-gvcf-gatk4');
-    await user.click(workflowCardButton);
-
-    const addButton = screen.getAllByRole('button').filter((button) => button.textContent.includes('Add to Workspace'))[0];
-    await user.click(addButton);
 
     // Assert
-    expect(toWorkspace).toHaveBeenCalledWith({ namespace, name }, methodConfiguration);
-    expect(goToPath).toHaveBeenCalledWith('workflow', { namespace, name, workflowName: 'haplotypecaller-gvcf-gatk4', workflowNamespace: 'gatk' });
+    expect(screen.getByRole('button', { name: /find a workflow/i })).toBeInTheDocument();
   });
 });
