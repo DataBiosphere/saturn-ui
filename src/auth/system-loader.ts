4b9c1dbc71a16dce5ec5f828c930a5c93a252835
Miguel Silva <msilva@broadinstitute.org>
2024-12-04 11:11:09 -0500

    UIE-204 Narrow Ajax Usage pt15 - shifting ajax usage to directly use ajax sub-areas for support and misc top-level uses. - improving mock types where possible.

--- src/auth/system-loader.ts
+++ src/auth/system-loader.ts
@@ -1,9 +1,9 @@
-import { Ajax } from 'src/libs/ajax';
+import { TermsOfService } from 'src/libs/ajax/TermsOfService';
 import { SamTermsOfServiceConfig } from 'src/libs/ajax/TermsOfService';
 import { SystemState, systemStore } from 'src/libs/state';
 
 export const initializeSystemProperties = async (): Promise<void> => {
-  const termsOfServiceConfig: SamTermsOfServiceConfig = await Ajax().TermsOfService.getTermsOfServiceConfig();
+  const termsOfServiceConfig: SamTermsOfServiceConfig = await TermsOfService().getTermsOfServiceConfig();
   systemStore.update((state: SystemState) => ({
     ...state,
     termsOfServiceConfig,
